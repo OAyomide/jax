78464fa9a1510f415523d7abd90ed8072d6f6964
Ayomide Onigbinde <onigbindeayomide@gmail.com>
2020-01-28 14:04:31 +0100

    all: init commit

---
+++ src/main/java/com/example/jax/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.jax;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    public String index() {
+        return "Hello there! This is my first spring REST API app";
+    }
+}
